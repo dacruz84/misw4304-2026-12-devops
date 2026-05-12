e188cca145ec5ddb06f06ae4735f37ea4c4354ea
David Cruz <da.cruz84@uniandes.edu.co>
2026-05-11 20:14:57 -0500

    Actualización del dockerfile y la versión de la app

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD ["gunicorn", "--bind", "0.0.0.0:5000", "application:app"]
+CMD ["newrelic-admin", "run-program", "gunicorn", "--bind", "0.0.0.0:5000", "application:app"]
--- application.py
+++ application.py
@@ -4,7 +4,7 @@ newrelic.agent.initialize('newrelic.ini')
 from dotenv import load_dotenv
 load_dotenv()
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 
 from flask import Flask
 from flask_restful import Api
