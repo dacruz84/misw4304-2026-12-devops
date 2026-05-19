ab5611eaba7e5eb56b95b11941ed60e55f0d4093
David Cruz <da.cruz84@uniandes.edu.co>
2026-05-19 17:42:53 -0500

    Incrementar la versión de la aplicación

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ newrelic.agent.initialize('newrelic.ini')
 from dotenv import load_dotenv
 load_dotenv()
 
-__version__ = "1.0.12"
+__version__ = "1.0.13"
 
 from flask import Flask
 from flask_restful import Api
